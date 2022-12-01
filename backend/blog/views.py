691d8966b12dd26719db69a7213b095b8a8a00d6
MosFazli <mostfafazli@gmail.com>
2022-12-01 12:57:35 -0500

    add Article model and viewsets

---
+++ backend/blog/views.py
@@ -0,0 +1,6 @@
+from django.views.generic import ListView
+from .models import Article
+
+class ArticleList(ListView):
+    def get_queryset(self):
+        return Article.objects.filter(status=True)
